9de3965ad50c2d3d8ec7e50382833e56b9d495c7
Arsyian Rizki Pratama <103777693+Arsyian-Rizki-Pratama@users.noreply.github.com>
2022-04-16 06:08:44 +0700

    menambahkan program konversi speech to text

--- speech-recognition.py
+++ speech-recognition.py
@@ -1,3 +1,19 @@
 #Mengambahkan Framework yang digunakan untuk speech recognition
 import speech_recognition as sr
 import pyttsx3
+
+#Program di bawah untuk membuat setelah tiga detik kita tidak bicara maka kalimat akan di rekam. default 0.8 sekon
+#engine.puse_treshold = 3
+
+with mic as source:
+    print("silahkan mulai bicara")
+    rekaman = engine.listen(source)
+    print("waktu habis")
+    
+    try:
+        hasil = engine.recognize_google(rekaman, language = "id-ID")
+        print(hasil)
+    #except engine.UnknownValueError:
+        #print("Maaf tidak dapat dideteksi, Silahkan Coba Lagi")
+    except Exception as e:
+        print(e)
